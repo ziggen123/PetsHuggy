6c8092b90b65fe22fd38eb21322e95acb02bc985
EC2 Default User <ec2-user@ip-172-31-28-122.ec2.internal>
2018-07-30 05:36:27 +0000

    Add gems for Heroku

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,10 @@
+module ApplicationHelper
+  def controller?(controller)
+    controller.include?(params[:controller]) #引数が現在のコントローラに含まれていたらtrueを返す
+  end
+
+  def action?(action)
+    action.include?(params[:action]) #引数が現在のアクションに含まれていたらtrueを返す
+  end
+  
+end
