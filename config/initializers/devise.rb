07afa99a04c55d6762db29c6eee460e342e4913b
EC2 Default User <ec2-user@ip-172-31-28-122.ec2.internal>
2018-08-02 06:09:09 +0000

    Add gems for Heroku

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -283,5 +283,6 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-  config.omniauth :facebook, ENV["facebook_app_id"], ENV["facebook_app_secret"], scope: 'email', info_fields: 'email,name'
+  #config.omniauth :facebook, ENV["facebook_app_id"], ENV["facebook_app_secret"], scope: 'email', info_fields: 'email,name'
+  config.omniauth :facebook, "612045949240691", "69734ff081beb4064eca1fd6e20db6fc", scope: 'email', info_fields: 'email,name'
 end
